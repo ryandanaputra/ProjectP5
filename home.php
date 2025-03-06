2da8f09fb4d05f4fa6c2d7c8af5e835483694ff5
ryandanaputra <puturyandana@gmail.com>
2025-03-06 09:02:53 +0800

    jadi halaman utama

--- home.php
+++ home.php
@@ -16,7 +16,7 @@
 <body>
     <section id="home">
         <div class="top-header">
-            <h1 class="top-header-text">Menu</h1>
+            <h1 class="top-header-text" id="top-header-text">Menu</h1>
             <img class="logo" src="img/logo.png" alt="logo">
         </div>
         <div class="search-bar">
@@ -24,54 +24,45 @@
             <input type="search" name="search" id="search">
         </div>
         <?php include 'pages/category-header.html'; ?>
-        <h1 class="top-header-text">Promo</h1>
-        <div class="container-contents">
-            <div class="promo-card">
-                <div class="promo-content">
-                    <h2>Penawaran Hari Ini</h2>
-                    <h1>Gratis Jajanan Bali</h1>
-                    <p>*minimal pembelian Rp50.000,00</p>
-                </div>
-                <div class="promo-image">
-                    <img src="img/kumpulan_makanan.png">
+        <div id="content-container">
+            <h1 class="top-header-text">Promo</h1>
+            <div class="container-contents">
+                <div class="promo-card">
+                    <div class="promo-content">
+                        <h2>Penawaran Hari Ini</h2>
+                        <h1>Gratis Jajanan Bali</h1>
+                        <p>*minimal pembelian Rp50.000,00</p>
+                    </div>
+                    <div class="promo-image">
+                        <img src="img/kumpulan_makanan.png">
+                    </div>
                 </div>
             </div>
-        </div>
-        <h1 class="top-header-text">Popular</h1>
-        <div class="popular-container">
-            <div class="popular-card">
-                <div class="popular-card-img">
-                    <img src="img/nasi_campur_bali.png" alt="">
+            <h1 class="top-header-text">Popular</h1>
+            <div class="popular-container">
+                <div class="popular-card">
+                    <div class="popular-card-img">
+                        <img src="img/nasi_campur_bali.png" alt="">
+                    </div>
+                    <div class="text-wrapper">
+                        <p class="food-card-name">Nasi Campur Bali</p>
+                        <p class="price-card">Rp 15.000,00</p>
+                    </div>
+                    <i class='bx bxs-plus-circle'></i>
                 </div>
-                <div class="text-wrapper">
-                    <p class="food-card-name">
-                        Nasi Campur Bali
-                    </p>
-                    <p class="price-card">
-                        Rp 15.000,00
-                    </p>
+                <div class="popular-card">
+                    <div class="popular-card-img">
+                        <img src="img/tipat_cantok.png" alt="">
+                    </div>
+                    <div class="text-wrapper">
+                        <p class="food-card-name">Tipat Cantok</p>
+                        <p class="price-card">Rp 25.000,00</p>
+                    </div>
+                    <i class='bx bxs-plus-circle'></i>
                 </div>
-                <i class='bx bxs-plus-circle'></i>
             </div>
-            <div class="popular-card">
-                <div class="popular-card-img">
-                    <img src="img/tipat_cantok.png" alt="">
-                </div>
-                <div class="text-wrapper">
-                    <p class="food-card-name">
-                        Tipat Cantok
-                    </p>
-                    <p class="price-card">
-                        Rp 25.000,00
-                    </p>
-                </div>
-                <i class='bx bxs-plus-circle'></i>
-            </div>
-        </div>
         </div>
     </section>
-
-
 
     <?php include 'pages/navbar.html' ?>
 </body>
